d65e8149aa8a17c1a978b1620876d16cb39ab531
idris989 <74424146+idris989@users.noreply.github.com>
2026-04-05 21:33:43 +0400

    Remove SQLite database access from view.js
    Removed database connection and user retrieval code.

--- view.js
+++ view.js
@@ -1,8 +1,1 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./skating.db');
 
-db.all("SELECT * FROM users", [], (err, rows) => {
-    if (err) return console.error(err.message);
-    console.table(rows); // <--- THIS WILL SHOW YOUR TABLE
-});
-db.close();
